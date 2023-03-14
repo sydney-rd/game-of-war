b710a1a60cd17fbff3e9e43370f6290c232c579c
sydney-rd <sydneydavid56@gmail.com>
2023-03-14 14:32:26 -0400

    Adding readme

--- README.md
+++ README.md
@@ -1,58 +1,23 @@
-[![General Assembly Logo](https://camo.githubusercontent.com/1a91b05b8f4d44b5bbfb83abac2b0996d8e26c92/687474703a2f2f692e696d6775722e636f6d2f6b6538555354712e706e67)](https://generalassemb.ly/education/web-development-immersive)
+# game-of-war
 
-# Game of War
+A simple card game, typically played by two players. The objective
+of the game is to win all of the cards. The deck will be divided evenly amongst players. Each player will reveal the top card from their deck and the player with the higher card takes both and places behind their stack
 
-Have you played the card game [War](https://www.youtube.com/watch?v=yX-jOVer758)? It's very simple! 
+WAR happens when both players draw a card of equal value. Players will then place three cards, face down, and one more card - face up. The player with the highest card, takes both piles 
 
-Two players evenly split a shuffled deck of 52 standard playing cards. 
-Each stack is facedown, and players cannot look at the cards in their stack or rearrange them.
-The objective of the game is to play until one player has all 52 cards in their stack.
+A winner is announced when ome player has 0 cards and the other, all 52
 
-Players take turns playing the top card from their stacks. 
-Whoever's card is a higher value puts both cards at the bottom of their stack.
+# requirements
 
-If there is a tie, then it's War! Each player adds three cards of their stack face down.
-Each player reveals their fourth card. Whoever wins that reveal takes all of the cards!
-If there is another tie the process repeats until there is a winner.
+check to see if you have Node.js installed
+run the following command: 
 
+node -v
 
-## Instructions
+otherwise, install using link:
+https://nodejs.org/en/download/
 
-1. Create a new repository on GitHub (not GitHub Enterprise).
-1. Clone down the repository and complete your work in there.
-1. Fulfill the listed requirements below.
+# run
 
-Please turn in your submission by the deadline on your cohort calendar. Submit
-your project by posting an issue on this repository with a link to your
-repository on GitHub (not GitHub Enterprise).
-
-
-### Technical Requirements
-
-Your project should meet the following requirements:
-
-- Your game should run without errors
-- The game starts immediately when the JavaScript is executed. Create your two players and start the rounds!
-- There is no user input. The program simply loops through rounds until the game is finished.
-- Print a message for each round showing the cards played by each user, who won the round, and how many cards each player has.
-- Include a README written in well-formatted Markdown (_hint: look up [README templates](https://www.readme-templates.com/)_)
-- Show a good commit history with frequent commits (We're looking for lots of small commits!)
-
-
-## Hints
-
-- Adding a property for the "value" of a rank could make it easier to compare face cards (ie: Jack < Queen). For example, 2-10 will represent each number and then 11, 12, 13, 14 for J Q K A, respectively.
-- It could be helpful to have a class to represent the overall Game, particularly when handling ties and other complicated game states.
-- Write Pseudocode!
-- Break the project down into different components. What are the pieces to this problem? Solve them each individually and build towards a solution for the overall problem. Teach each piece individually before moving on!
-- Make it work, make it good, make it fast. This is a common developer axiom. It means: focus first on making something work (even if it isn't well written or it's buggy). Then, turn to making it work well. Then, and only then, start to worry about other things like performance.
-
-## Plagiarism
-
-Take a moment to refamiliarize yourself with the [Plagiarism policy](https://git.generalassemb.ly/DC-WDI/Administrative/blob/master/plagiarism.md). Plagiarized work will not be accepted.
-
-## [License](LICENSE)
-
-1.  All content is licensed under a CC­BY­NC­SA 4.0 license.
-1.  All software code is licensed under GNU GPLv3. For commercial use or
-    alternative licensing, please contact legal@ga.co.
+in your terminal:
+node script.js
---
+++ script.js
@@ -0,0 +1,110 @@
+// game of war class 
+class gameOfWar {
+    constructor() {
+        this.p1 = [];
+        this.p2 = [];
+        this.deck = new Deck();
+        this.shuffleDeck();
+        this.splitDeck();
+        this.pile = [];
+    }
+    shuffleDeck() {
+        for (let i = this.deck.cards.length - 1; i > 0; i-- ) {
+            let j = Math.floor(Math.random() * i);
+            let temp = this.deck.cards[i];
+            this.deck.cards[i] = this.deck.cards[j];
+            this.deck.cards[j] = temp;
+        }
+    }
+    splitDeck() {
+        let splitDeck = Math.ceil(this.deck.cards.length/2);
+        let halfDeck = this.deck.cards.splice(0, splitDeck);
+        this.p1.push(...halfDeck);
+        this.p2.push(...this.deck.cards);
+        delete this.deck // deletes original array holding 52 cards
+    }
+    
+    startGame() {
+        // while loop until a player has 0 cards left 
+        while (this.p1.length !== 0 && this.p2.length !== 0 ) {
+        let p1Card = this.p1.pop();
+        let p2Card = this.p2.pop();
+        if (p1Card.score === p2Card.score) {  
+            console.log(`PLAYER 1: ${p1Card.suit} of ${p1Card.rank} with ${this.p1.length} amt of cards -- PLAYER 2: ${p2Card.suit} of ${p2Card.rank} with ${this.p2.length} amt of cards...WAR!`);
+            this.pile.push(p1Card, p2Card);
+            this.war();
+        } else if (p1Card.score > p2Card.score) { 
+            console.log(`PLAYER 1: ${p1Card.suit} of ${p1Card.rank} with ${this.p1.length} amt of cards -- PLAYER 2: ${p2Card.suit} of ${p2Card.rank} with ${this.p2.length} amt of cards -- PLAYER 1 wins this round!`);
+            this.p1.unshift(p2Card, p1Card, ...this.pile);
+            this.pile.length = 0; // clear pile 
+        } else {
+            console.log(`PLAYER 1: ${p1Card.suit} of ${p1Card.rank} with ${this.p1.length} amt of cards -- PLAYER 2: ${p2Card.suit} of ${p2Card.rank} with ${this.p2.length} amt of cards -- PLAYER 2 wins this round!`);
+            this.p2.unshift(p1Card, p2Card, ...this.pile);
+            this.pile.length = 0; // clear pile     
+        }
+    }
+    // winning conditionals when while loop breaks
+    if (this.p1.length === 0) {
+        this.p1.unshift(...this.pile)
+        this.pile.length = 0; // clear pile     
+        console.log(`Player 2 wins entire game, they have ${this.p2.length} cards`); 
+    } else if (this.p2.length === 0) {
+        this.p2.unshift(...this.pile)
+        this.pile.length = 0; // clear pile 
+        console.log(`Player 1 wins entire game, they have ${this.p1.length} cards`); 
+    }
+    
+}
+    // player 1 and 2 are not having the cards push properly !!!
+    war() {
+        if (this.p1.length <= 3) {
+            // player 1 loses
+            this.p1.length = 0
+        } else if (this.p2.length <= 3) {
+            // player 2 loses
+            this.pile.push(...this.p2)
+            this.p2.length = 0
+        } else {
+            this.pile.push(...this.p1.splice(this.p1.length - 3, 3)); // removes 3 cards
+            this.pile.push(...this.p2.splice(this.p2.length - 3, 3));
+        }
+    }
+}
+
+// creates a car with suit, rank, and score
+class Card {
+    constructor(suit, rank, score) {
+        this.suit = suit;
+        this.rank = rank;
+        this.score = score;
+    }
+}
+
+// creates a deck by pushing a suit, rank and its value to each card equaling 52
+class Deck {
+    constructor() {
+        const suits = ["Hearts", "Clubs", "Spades", "Diamonds"];
+        const rank = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
+        this.cards = [];
+        for (let i = 0; i < rank.length; i++) {
+            for (let j = 0; j < suits.length; j++) {
+                this.cards.push(new Card(suits[j], rank[i], i+1));
+            }
+        }
+    }
+    
+}
+
+// test code
+// calls card and deck class
+// const test_deck = new Deck();
+// console.log(test_deck)
+
+// calls and tests the split function
+// const game = new Game();
+// console.log(game.p1);
+// console.log("split")
+// console.log(game.p2);
+let game = new gameOfWar();
+// game1.gameInit();
+game.startGame();
